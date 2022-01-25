d3ee04d106f120892cd57f9bb202a75670243b44
JHoweWowe <howejust@gmail.com>
2022-01-25 13:43:55 +0000

    Create basic outline of web app

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
-from bson import json_util, ObjectId
-import json
-
-from flask import Flask, render_template
+from flask import Flask, render_template, Response, request, redirect
 from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+
+import json
 
 app = Flask(__name__)
 mongo = PyMongo(app, uri="mongodb://localhost:27017/honours-proj-website-recipes")
@@ -11,9 +11,30 @@ mongo = PyMongo(app, uri="mongodb://localhost:27017/honours-proj-website-recipes
 def index():
     return render_template('index.html')
 
-@app.route('/test')
-def meow():
-    test = mongo.db.bbcgoodfood.find({"title": "Halloumi flatbreads"})
-    first = list(test)[0]
-    page_sanitized = json.loads(json_util.dumps(first))
-    return page_sanitized
+# Test with filters as arguments
+@app.route('/', methods=['POST'])
+def search_recipes():
+    filters = []
+    time = request.form['cooking-time']
+    print(time)
+    #return get_recipe_data(time)
+    return test_get_recipe_data(time)
+
+@app.route('/test-route-recipes', methods=['GET'])
+def test_get_recipe_data(time):
+    #return render_template('comments.html', comments=test_comments)
+    try:
+        data = list(mongo.db.bbcgoodfood.find({"time": time})) # Takes recipes with 50 mins
+        for recipe in data:
+            recipe["_id"] = str(recipe["_id"])
+        return render_template('recipes.html', data=data)
+
+    except Exception as ex:
+        print(ex)
+        return Response(response= json.dumps({"message": "cannot read recipes"}), status=500, mimetype="application/json")
+
+# Routing for a specific recipe - perhaps add it as URL, pass recipe as parameter
+@app.route('/recipe/<id>', methods=['GET'])
+def view_recipe(id):
+    recipe_data = mongo.db.bbcgoodfood.find_one({"_id": ObjectId(id)})
+    return render_template('recipe.html', id=id, recipe_data = recipe_data) # Data passed redudantly
--- web_scrapping_module.py
+++ web_scrapping_module.py
@@ -3,20 +3,17 @@ import pymongo
 from pymongo import MongoClient
 
 client = MongoClient('localhost', 27017)
-
 recipe_db = client['honours-proj-website-recipes']
 test_recipe_collection = recipe_db['bbcgoodfood'] # By default should only collect recipes from BBCGoodFood
-
-
 
 # Setup basic web scrapping
 from bs4 import BeautifulSoup
 import requests
 
 # TODO: Web scrape multiple recipes from student recipes and obtain URL for each if applicable
-test_url = 'https://www.bbcgoodfood.com/recipes/collection/student-recipes'
+base_url = 'https://www.bbcgoodfood.com/recipes/collection/student-recipes'
 
-html_text = requests.get(test_url).text
+html_text = requests.get(base_url).text
 soup = BeautifulSoup(html_text, features='html.parser') # Indicate HTML parser 
 recipes = soup.findAll('li', class_ = 'dynamic-list__list-item list-item')
 
@@ -24,27 +21,50 @@ recipes_json_list = []
 
 # NOTE: For BBCGoodFood, only loads information up to a certain page - should also have unique id associated to each recipe
 for recipe in recipes:
-    url = recipe.find('a', class_ = 'link d-block').get('href')
+    specific_recipe_url = recipe.find('a', class_ = 'link d-block').get('href')
 
     title = recipe.find('h2', class_ = 'd-inline heading-4').text
     description = recipe.find('p', class_ = 'd-block body-copy-small').text # NOTE: Should it be stored???
+    
+    image_url = recipe.find('img', class_ = 'image__img')['src']
 
-    cooking_time = recipe.find('span', class_ = 'terms-icons-list__text d-flex align-items-center')
+    cooking_time = recipe.find('span', class_ = 'terms-icons-list__text d-flex align-items-center').text
     cooking_difficulty = recipe.find('span', class_ = 'terms-icons-list__text d-flex align-items-center')
 
-    # jialat how to find ingredients - from specific url can
-    ingredients = None
+
+    # jialat specifically find information from individual recipe url can
+    individual_recipe_url = base_url + specific_recipe_url
+    individual_html_recipe_text = requests.get(individual_recipe_url).text
+    soup = BeautifulSoup(individual_html_recipe_text, features='html.parser')
+    individual_recipe_itself = soup.find('div', class_='post recipe')
+    ingredients = individual_recipe_itself.find('div', class_ = 'row recipe__instructions')
+
+    # Obtain ingredients
+    list_of_ingredients_array = []
+    test = ingredients.find('ul', class_='list')
+    for t in test:
+        list_of_ingredients_array.append(t.text)
+
+    # Obtain steps
+    steps_array = []
+    test2 = ingredients.find('ul', class_='grouped-list__list list')
+    for t2 in test2:
+        steps_array.append(t2.p.text)
+
 
     # TODO: Add ingredients
     recipe_json = {
         "title": title,
-        "description": description
+        "description": description,
+        "image_url": image_url,
+        "time": cooking_time,
+        "ingredients": list_of_ingredients_array,
+        "steps": steps_array
     }
 
     recipes_json_list.append(recipe_json)
 
-# For debugging purposes
-#print(recipes_json_list)
+#print(recipes_json_list) # For debugging purposes
 
 # Bad code practice to store and conduct bulk queries...
 result = test_recipe_collection.insert_many(recipes_json_list)
